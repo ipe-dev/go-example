0fde0399f0e7d3c41c0c4b05531405acb0ff934f
ipe <izaki.yb1998@gmail.com>
2024-03-16 21:12:06 +0900

    自動オプションの設定

--- httprouter/main.go
+++ httprouter/main.go
@@ -18,6 +18,15 @@ func Hello(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 
 func main() {
 	router := httprouter.New()
+	// Automatic OPTIONS
+	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Access-Control-Request-Method") != "" {
+			header := w.Header()
+			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
+			header.Set("Access-Control-Allow-Origin", "*")
+		}
+		w.WriteHeader(http.StatusNoContent)
+	})
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 	log.Fatal(http.ListenAndServe(":8080", router))
