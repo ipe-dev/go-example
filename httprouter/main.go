22870e79bd251e4e98bb47dadfd19a30791d462a
ipe <izaki.yb1998@gmail.com>
2024-03-19 07:06:02 +0900

    options

--- httprouter/main.go
+++ httprouter/main.go
@@ -20,11 +20,13 @@ func main() {
 	router := httprouter.New()
 	// Automatic OPTIONS
 	router.GlobalOPTIONS = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		header := w.Header()
 		if r.Header.Get("Access-Control-Request-Method") != "" {
-			header := w.Header()
 			header.Set("Access-Control-Allow-Methods", header.Get("Allow"))
-			header.Set("Access-Control-Allow-Origin", "*")
+			header.Set("Access-Control-Allow-Origin", "http://localhost:8080")
+			header.Set("Access-Control-Max-Age", "3600")
 		}
+		header.Set("hogehoge", "piyo")
 		w.WriteHeader(http.StatusNoContent)
 	})
 	router.GET("/", Index)
