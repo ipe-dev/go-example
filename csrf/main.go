e720b013cfb593f64ac80b6439f1eddf36e849d0
ipe <izaki.yb1998@gmail.com>
2024-03-29 07:09:10 +0900

    csrf wip

--- csrf/main.go
+++ csrf/main.go
@@ -1,17 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 )
+
 type PostRequest struct {
 	Name string `json:"name"`
 }
-func postHello(c echo.Context) {
-	request := c.Bind(i interface{})
-	return c.JSON(http.StatusOK, )
+
+func postHello(c echo.Context) error {
+	r := new(PostRequest)
+	if err := c.Bind(r); err != nil {
+		return err
+	}
+	return c.JSON(http.StatusOK, map[string]string{"message": fmt.Sprintf("hello %s", r.Name)})
 }
 func main() {
-
+	e := echo.New()
+	
+	e.POST("/hello", postHello)
+	e.Logger.Fatal(e.Start(":8080"))
 }
