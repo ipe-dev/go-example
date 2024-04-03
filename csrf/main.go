6279129ec641b3920374d65dca6f6a1b2e8101c1
ipe <izaki.yb1998@gmail.com>
2024-04-03 23:20:31 +0900

    csrf wip

--- csrf/main.go
+++ csrf/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"net/http"
-
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
@@ -13,23 +12,32 @@ type PostRequest struct {
 }
 
 func postHello(c echo.Context) error {
-	fmt.Println("hoge")
+	if err := c.Ech; err != nil {
+		return echo.ErrForbidden
+	}
 	r := new(PostRequest)
 	if err := c.Bind(r); err != nil {
 		return err
 	}
 	return c.JSON(http.StatusOK, map[string]string{"message": fmt.Sprintf("hello %s", r.Name)})
 }
+func csrfToken(c echo.Context) error {
+	token := c.Get("csrf")
+	fmt.Println(token)
+	return c.JSON(http.StatusOK, map[string]string{"token": token.(string)})
+}
 func main() {
 	e := echo.New()
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowCredentials: true,
 	}))
-	// e.Use(middleware.CSRF())
+
 	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
 		TokenLookup: "header:X-XSRF-TOKEN",
 	}))
+	e.GET("/csrf_token", csrfToken)
 	e.POST("/hello", postHello)
 	e.Logger.Fatal(e.Start(":8080"))
+
 }
